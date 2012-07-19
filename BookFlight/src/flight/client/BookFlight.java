ca384e0cd7d29c3da156fda17799391aeb471d27
creakywall <claytonwalker11@gmail.com>
2012-07-18 20:45:31 -0600

    Added some validation

--- BookFlight/src/flight/client/BookFlight.java
+++ BookFlight/src/flight/client/BookFlight.java
@@ -1,5 +1,6 @@
 package flight.client;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import flight.shared.FieldVerifier;
@@ -59,10 +60,10 @@ public class BookFlight implements EntryPoint {
 	private Label toBoxLabel = new Label("To Airport");
 	private TextBox toBox = new TextBox();
 
-	private Label leaveBoxLabel = new Label("Leave (mm/dd/yyyy)");
+	private Label leaveBoxLabel = new Label("Leave (yyyy-mm-dd)");
 	private TextBox leaveBox = new TextBox();
 
-	private Label returnBoxLabel = new Label("Return (mm/dd/yyyy)");
+	private Label returnBoxLabel = new Label("Return (yyyy-mm-dd)");
 	private TextBox returnBox = new TextBox();
 
 	/**
@@ -89,6 +90,11 @@ public class BookFlight implements EntryPoint {
 		pListBox.addItem("3", "3");
 		pListBox.addItem("4", "4");
 		pListBox.addItem("5", "5");
+		pListBox.addItem("6", "6");
+		pListBox.addItem("7", "7");
+		pListBox.addItem("8", "8");
+		pListBox.addItem("9", "9");
+		pListBox.addItem("10", "10");
 		fTable.setWidget(4, 1, pListBox);
 
 		fTable.setWidget(5, 0, search);
@@ -103,6 +109,7 @@ public class BookFlight implements EntryPoint {
 					cal1pop.setPopupPosition(cal1.getAbsoluteLeft() + 20,
 							cal1.getAbsoluteTop() + 20);
 					cal1pop.show();
+					cal2pop.hide();
 				} else {
 					cal1pop.hide();
 				}
@@ -115,6 +122,7 @@ public class BookFlight implements EntryPoint {
 					cal2pop.setPopupPosition(cal2.getAbsoluteLeft() + 20,
 							cal2.getAbsoluteTop() + 20);
 					cal2pop.show();
+					cal1pop.hide();
 				} else {
 					cal2pop.hide();
 				}
@@ -125,7 +133,8 @@ public class BookFlight implements EntryPoint {
 				Date date = (Date) event.getValue();
 				PredefinedFormat pdf = PredefinedFormat.DATE_SHORT;
 				String dateString = DateTimeFormat.getFormat(pdf).format(date);
-				//String dateString = DateTimeFormat.getShortDateFormat().format(date);
+				// String dateString =
+				// DateTimeFormat.getShortDateFormat().format(date);
 				leaveBox.setText(dateString);
 				cal1pop.hide();
 			}
@@ -136,7 +145,8 @@ public class BookFlight implements EntryPoint {
 				Date date = (Date) event.getValue();
 				PredefinedFormat pdf = PredefinedFormat.DATE_SHORT;
 				String dateString = DateTimeFormat.getFormat(pdf).format(date);
-				//String dateString = DateTimeFormat.getShortDateFormat().format(date);
+				// String dateString =
+				// DateTimeFormat.getShortDateFormat().format(date);
 				returnBox.setText(dateString);
 				cal2pop.hide();
 			}
@@ -147,7 +157,7 @@ public class BookFlight implements EntryPoint {
 
 		tpanel.setWidth("600px");
 		tpanel.add(vpanel, "Book Flights");
-		tpanel.add(new VerticalPanel(), "Register");
+		tpanel.add(new VerticalPanel(), "Check In");
 		tpanel.selectTab(0);
 
 		RootPanel.get("bookFlight").add(tpanel);
@@ -156,20 +166,21 @@ public class BookFlight implements EntryPoint {
 			public void onClick(ClickEvent e) {
 				String fromAirport = fromBox.getText().trim();
 				String toAirport = toBox.getText().trim();
-				
+
 				String searchStart = leaveBox.getText().trim();
 				String searchEnd = returnBox.getText().trim();
-				
+
 				if (fromAirport.length() == 0) {
-					Window.alert("Origin airport is required");
-				} else if( toAirport.length() == 0){
-					Window.alert("Destination airport is required");
+					Window.alert("'From' airport is required");
+				} else if (toAirport.length() == 0) {
+					Window.alert("'To' airport is required");
 				} else if (searchStart.length() == 0) {
 					Window.alert("Departure date was not provided");
 				} else if (searchEnd.length() == 0) {
 					Window.alert("Arrival date was not provided");
 				} else {
-					//DateTimeFormat f = DateTimeFormat.getFormat("YYYY/MM/dd");
+					// DateTimeFormat f =
+					// DateTimeFormat.getFormat("YYYY/MM/dd");
 					PredefinedFormat pdf = PredefinedFormat.DATE_SHORT;
 					DateTimeFormat f = DateTimeFormat.getFormat(pdf);
 					Date dstart = null;
@@ -187,11 +198,30 @@ public class BookFlight implements EntryPoint {
 					if ((dstart != null) && (dend != null)) {
 						if (dstart.after(dend)) {
 							Window.alert("Departure date cannot be after arrival date");
+						} else if (dstart.before(now())) {
+							Window.alert("Departure date cannot be in the past");
+						} else if (dend.before(now())) {
+							Window.alert("Arrival date cannot be in the past");
 						}
-					}
+						else{
+							bookFlight();
+						}
+					} 
 				}
 			}
 		});
 
+	}
+
+	public static Date now() {
+
+		Date date = new Date();
+
+		return date;
+
+	}
+
+	private void bookFlight() {
+		Window.alert("Your flight is booked.");
 	}
 }
